3d67182241c5559a596a30a43347922ee345f5a6
Matthew Little <matt@pingman.com>
2017-01-25 13:54:03 -0700

    No longer force 32bit process mode with runtime loading of either 32/64 bit lib depending on process runtime architecture

--- LiteHtmlSharp/PInvokes.cs
+++ LiteHtmlSharp/PInvokes.cs
@@ -15,15 +15,28 @@ namespace LiteHtmlSharp
 #if __MonoCS__
       const string LiteHtmlLibFile = "litehtml";
       public const CharSet cs = CharSet.Ansi;
-#elif WIN64
-      const string LiteHtmlLibFile = "x64\\LiteHtmlLib.dll";
-      public const CharSet cs = CharSet.Unicode;
+
 #else
-      const string LiteHtmlLibFile = "x86\\LiteHtmlLib.dll";
+      const string LiteHtmlLibFile = "LiteHtmlLib.dll";
       public const CharSet cs = CharSet.Unicode;
 #endif
 
+      const string LiteHtmlLibFile_x64 = "x64\\LiteHtmlLib.dll";
+      const string LiteHtmlLibFile_x86 = "x86\\LiteHtmlLib.dll";
+
       public const CallingConvention cc = CallingConvention.Cdecl;
+
+      static PInvokes()
+      {
+         if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+         {
+            var is64Bit = Environment.Is64BitProcess;
+            LoadLibrary(is64Bit ? LiteHtmlLibFile_x64 : LiteHtmlLibFile_x86);
+         }
+      }
+
+      [DllImport("kernel32.dll")]
+      private static extern IntPtr LoadLibrary(string dllToLoad);
 
       [DllImport(LiteHtmlLibFile, CallingConvention = cc, SetLastError = true)]
       public static extern void Init(ref DocumentCalls document, InitCallbacksFunc initFunc);
