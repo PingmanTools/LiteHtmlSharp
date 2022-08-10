e19c08ba202f37a82526bafb6dee2f19e5b82d63
Tig Kindel <tig@users.noreply.github.com>
2022-08-10 16:35:54 -0600

    Fixes bulding with latest litehaml where src/include dirs were separated

--- LiteHtmlLib/src/DocContainer.h
+++ LiteHtmlLib/src/DocContainer.h
@@ -23,7 +23,7 @@ public:
 	virtual void delete_font(litehtml::uint_ptr hFont) override;
 	virtual int text_width(const litehtml::tchar_t * text, litehtml::uint_ptr hFont) override;
 	virtual void draw_text(litehtml::uint_ptr hdc, const litehtml::tchar_t * text, litehtml::uint_ptr hFont, litehtml::web_color color, const litehtml::position & pos) override;
-	virtual int pt_to_px(int pt) override;
+	virtual int pt_to_px(int pt) const override;
 	virtual int get_default_font_size() const override;
 	virtual const litehtml::tchar_t * get_default_font_name() const override;
 	virtual void draw_list_marker(litehtml::uint_ptr hdc, const litehtml::list_marker & marker) override;
