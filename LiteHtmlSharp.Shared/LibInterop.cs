423cc967fb9005af539af34faf5cf9693b70de5c
Connor Lee <connor@pingman.com>
2025-06-27 14:36:27 -0600

    Bump version to preview2.
    Use updated .NET9 native lib interop.
    - Created runtimes/{rid}/native/ folders for win-x64, win-x86, osx-x64, osx-arm64
    - Moved native libraries to standard runtime identifier locations
    - Windows: LiteHtmlLib.dll for x86/x64 architectures
    - macOS: liblitehtml.dylib universal binary for Intel/Apple Silicon

--- LiteHtmlSharp.Shared/LibInterop.cs
+++ LiteHtmlSharp.Shared/LibInterop.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -12,37 +13,34 @@ namespace LiteHtmlSharp
 {
     public class LibInterop : ILibInterop
     {
-
-#if WINDOWS
-        const string LiteHtmlLibFile = "LiteHtmlLib.dll";
+        const string LiteHtmlLibFile = "liblitehtml";
         public const CharSet cs = CharSet.Unicode;
-#else
-        const string LiteHtmlLibFile = "litehtml";
-        public const CharSet cs = CharSet.Ansi;
-#endif
-
-        const string LiteHtmlLibFile_x64 = "x64\\LiteHtmlLib.dll";
-        const string LiteHtmlLibFile_x86 = "x86\\LiteHtmlLib.dll";
-
         public const CallingConvention cc = CallingConvention.Cdecl;
 
         readonly static Lazy<LibInterop> _instance = new Lazy<LibInterop>(() => new LibInterop());
         public static LibInterop Instance => _instance.Value;
 
+        static LibInterop()
+        {
+            NativeLibrary.SetDllImportResolver(typeof(LibInterop).Assembly, DllImportResolver);
+        }
+
         LibInterop()
         {
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+        }
+
+        private static IntPtr DllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+        {
+            if (libraryName == LiteHtmlLibFile)
             {
-                LoadLibrary(Environment.Is64BitProcess ? LiteHtmlLibFile_x64 : LiteHtmlLibFile_x86);
+                return NativeLibrary.Load(LiteHtmlLibFile, assembly, searchPath);
             }
+            return IntPtr.Zero;
         }
 
         public void InitDocument(ref DocumentCalls document, InitCallbacksFunc initFunc) => Init(ref document, initFunc);
 
         public Utf8Str LibEchoTest(Utf8Str testStr) => EchoTest(testStr);
-
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr LoadLibrary(string dllToLoad);
 
         [DllImport(LiteHtmlLibFile, CallingConvention = cc, SetLastError = true)]
         static extern void Init(ref DocumentCalls document, InitCallbacksFunc initFunc);
